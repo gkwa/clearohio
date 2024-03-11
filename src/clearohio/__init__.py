ae5f95f0238bff9f583a3dacf8f144390174fdc6
Taylor Monacelli <taylormonacelli@gmail.com>
2024-03-11 15:33:35 -0700

    Add subcommand 'work'

--- src/clearohio/__init__.py
+++ src/clearohio/__init__.py
@@ -1,12 +1,27 @@
 import click
 
 
-@click.command()
+@click.group()
+def main():
+    """A simple Python click app."""
+
+
+@main.command()
 @click.option("--name", prompt="Enter your name", help="The name of the user")
 @click.option("--age", type=int, prompt="Enter your age", help="The age of the user")
-def main(name, age):
-    """A simple Python click app that greets the user."""
+def greet(name, age):
+    """Greets the user."""
     click.echo(f"Hello, {name}! You are {age} years old.")
+
+
+@main.command()
+@click.option("--fake", is_flag=True, help="Enable fake mode")
+def work(fake):
+    """Performs work."""
+    if fake:
+        click.echo("Performing fake work...")
+    else:
+        click.echo("Performing real work...")
 
 
 if __name__ == "__main__":
